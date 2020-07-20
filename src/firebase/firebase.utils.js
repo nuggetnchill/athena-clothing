03a2eac304d5ebafd1aac27d9e70cf43b484a9c1
Justin Nguyen <nuggetnchill@gmail.com>
2020-07-19 19:28:40 -0500

    intergrated firebase into app

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyAEahqfKPDyyoXnkAf1mC5afagJohzTFqA",
+    authDomain: "athena-clothing-db.firebaseapp.com",
+    databaseURL: "https://athena-clothing-db.firebaseio.com",
+    projectId: "athena-clothing-db",
+    storageBucket: "athena-clothing-db.appspot.com",
+    messagingSenderId: "773395463582",
+    appId: "1:773395463582:web:f2e2385cbdbcbe4415ea8f",
+    measurementId: "G-1090ZBM2XJ"
+  };
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: 'select_account' });
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
